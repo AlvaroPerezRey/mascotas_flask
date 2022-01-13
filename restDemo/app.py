387a2ccd0b79691406832b2f321c17da7d887458
Javier Ortega <fjortegan@gmail.com>
2022-01-13 21:30:06 +0100

    Initial commit

---
+++ restDemo/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+from users import blueprint as api_user
+from model import init_db
+
+app = Flask(__name__)
+init_db(app)
+app.register_blueprint(api_user, url_prefix="/api/")
+
+@app.route("/")
+def home():
+    return app.send_static_file("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
